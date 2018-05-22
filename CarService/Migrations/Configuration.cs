dae15489d253fe1d8177c8557f635848971e46d1
Ekaterina <katya1995g@yandex.ru>
2018-05-22 08:43:38 +0300

    init

---
+++ CarService/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace CarService.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CarService.Models.ApplicationContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CarService.Models.ApplicationContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
